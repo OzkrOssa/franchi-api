e8c78edc674aaac41d126dc23859db32fe6fb01b
Oscar Ossa <oskrlex17@gmail.com>
2025-04-17 15:36:39 -0500

    fix: correct error message for ErrNoUpdatedData in error.go

--- internal/core/domain/error.go
+++ internal/core/domain/error.go
@@ -7,4 +7,5 @@ var (
 	ErrConflictingData = errors.New("data conflicts with existing data in unique column")
 	ErrDataNotFound    = errors.New("data not found")
 	ErrNoUpdatedData   = errors.New("no data to updated")
+	ErrInvalidData     = errors.New("invalid data")
 )
