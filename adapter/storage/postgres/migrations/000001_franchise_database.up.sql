d2dce490001d16038187fefd3a8b085c966b1b42
Oscar Ossa <oskrlex17@gmail.com>
2025-04-17 13:33:01 -0500

    feat: add initial migration scripts for franchise, branch, and product tables

---
+++ adapter/storage/postgres/migrations/000001_franchise_database.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE franchises (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE branches (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    franchise_id BIGINT NOT NULL,
+    FOREIGN KEY (franchise_id) REFERENCES franchises(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0,
+    branch_id BIGINT NOT NULL,
+    FOREIGN KEY (branch_id) REFERENCES branches(id) ON DELETE CASCADE
+);
