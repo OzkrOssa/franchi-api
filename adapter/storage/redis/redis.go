b0106e3cfdd1c464c31d33afdd93226b99988015
Oscar Ossa <oskrlex17@gmail.com>
2025-04-17 13:35:25 -0500

    feat: implement Redis cache adapter with basic CRUD operations and utility functions

---
+++ adapter/storage/redis/redis.go
@@ -0,0 +1,49 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/OzkrOssa/franchi-api/adapter/config"
+	"github.com/OzkrOssa/franchi-api/internal/core/port"
+	"github.com/redis/go-redis/v9"
+)
+
+type Redis struct {
+	client *redis.Client
+}
+
+func New(ctx context.Context, config *config.Cache) (port.CacheRepository, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", config.Host, config.Port),
+		Username: config.User,
+		Password: config.Password,
+		DB:       0,
+	})
+
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Redis{client}, nil
+}
+
+func (r *Redis) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r *Redis) Get(ctx context.Context, key string) ([]byte, error) {
+	res, err := r.client.Get(ctx, key).Result()
+	bytes := []byte(res)
+	return bytes, err
+}
+
+func (r *Redis) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
+
+func (r *Redis) Close() error {
+	return r.client.Close()
+}
---
+++ internal/core/port/cache.go
@@ -0,0 +1,16 @@
+package port
+
+import (
+	"context"
+	"time"
+)
+
+type CacheRepository interface {
+	Set(ctx context.Context, key string, value []byte, ttl time.Duration) error
+
+	Get(ctx context.Context, key string) ([]byte, error)
+
+	Delete(ctx context.Context, key string) error
+
+	Close() error
+}
